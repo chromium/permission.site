e1a14d239d58bc9a5a961c955f240cf8be9b8297
Lucas Garron <lgarron@chromium.org>
2016-11-15 00:37:17 -0800

    Add popup.

--- index.js
+++ index.js
@@ -222,6 +222,24 @@ window.addEventListener("load", function() {
         interceptCopy = false;
       };
     }()),
+    "popup": function() {
+      var w = window.open(
+        location.href,
+        "Popup",
+        "resizable,scrollbars,status"
+      )
+      displayOutcome("popup", w ? "success" : "error")(w);
+    },
+    "popup-delayed": function() {
+      setTimeout(function() {
+        var w = window.open(
+          location.href,
+          "Popup",
+          "resizable,scrollbars,status"
+        )
+        displayOutcome("popup-delayed", w ? "success" : "error")(w);
+      }, 2000);
+    },
     "fullscreen": function() {
       // Note: As of 2014-12-16, fullscreen only allows "ask" and "allow" in Chrome.
       document.body.requestFullScreen(
