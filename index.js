0b9c0e27e5cbf895c28cad483f990420ee46175d
Vincent Scheib <scheib@users.noreply.github.com>
2017-06-27 13:09:02 -0700

    Use 'acceptAllDevices' for Bluetooth.
    On this site we do not care what devices are found. Using the acceptAllDevices
    option increases the chance that the Bluetooth chooser finds any device at all.

--- index.js
+++ index.js
@@ -135,7 +135,9 @@ window.addEventListener("load", function() {
     },
     "bluetooth": function() {
       navigator.bluetooth.requestDevice({
-        filters: [{services: ['battery_service']}]
+        // filters: [...] <- Prefer filters to save energy & show relevant devices.
+        // acceptAllDevices here ensures dialog can populate, we don't care with what.
+        acceptAllDevices:true
       }).then(
         displayOutcome("bluetooth", "success"),
         displayOutcome("bluetooth", "error")
