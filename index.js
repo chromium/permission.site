21044d7bc2ad41146a16e56db5a3f44b03ed7338
Subhradeep10 <subhradeepsamanta2002@gmail.com>
2021-09-09 11:14:38 +0530

    Dark mode icon is added

--- index.js
+++ index.js
@@ -584,8 +584,3 @@ window.addEventListener("load", function () {
   }
 
 });
-
-var icon = document.getElementById("icon")
-icon.onclick = function () {
-  document.body.classList.toggle("dark-theme");
-}
