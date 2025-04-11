a214814204fd2796fb72c223e7bd53901478547a
panmari <smmuzi@gmail.com>
2025-04-11 12:57:20 +0200

    Add granted handlers for each pepc type.
    In order to check if an immediate call to the underlying API succeeds. In the past, we saw some issues on MacOS, which cached the permission state internally.

--- pepc.js
+++ pepc.js
@@ -1,20 +1,53 @@
-const PEPC_TYPES = [
-    'camera',
-    'microphone',
-    'camera-microphone',
-    'geolocation'
-]
+const API_ACCESS_STATUSES = {
+    success: '🟢 success',
+    error: '🔴 error',
+    unknown: '⚫️ unknown',
+};
+// Display the feature access status (whether the feature can actually be accessed successfully in the browser)
+function updateAccessStatus(permissionName, accessStatus, message) {
+    const textToDisplay = API_ACCESS_STATUSES[accessStatus];
+    document.querySelector(`#${permissionName}-access-status`).innerText =
+        textToDisplay;
+    if (message) {
+        document.querySelector(`#${permissionName}-error-message`).innerText = message
+    }
+}
+// Utils
+function successCallback(permissionName) {
+    return () => updateAccessStatus(permissionName, 'success');
+}
+function errorCallback(permissionName) {
+    return (error) => updateAccessStatus(permissionName, 'error', error.message);
+}
+
+const PEPC_TYPES_WITH_GRANTED_HANDLER = {
+    'camera': () => { },
+    'microphone': () => { },
+    'camera-microphone': () => { },
+    'geolocation': function () {
+        navigator.geolocation.getCurrentPosition(
+            successCallback('geolocation'),
+            errorCallback('geolocation')
+        );
+    }
+}
 
 window.addEventListener("load", function () {
-    PEPC_TYPES.forEach((id) => {
-        const pepc = document.getElementById(id);
+    for (const [type, grantedHandler] of Object.entries(PEPC_TYPES_WITH_GRANTED_HANDLER)) {
+        const pepc = document.getElementById(type);
 
         pepc.addEventListener('promptdismiss', function () {
             console.log('Dismiss, permission status: ' + pepc.permissionStatus);
+            if (pepc.permissionStatus == 'granted') {
+                grantedHandler();
+            }
         });
 
         pepc.addEventListener('promptaction', function () {
             console.log('Action, permission status: ' + pepc.permissionStatus);
+            if (pepc.permissionStatus == 'granted') {
+                grantedHandler();
+            }
         });
-    });
+    }
 });
